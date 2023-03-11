c629af22e3d5746f409f1e35eae67b536ce3d7cd
unknown <lucas11souza97@gmail.com>
2023-03-11 17:06:47 -0400

    created project

---
+++ resources/hotel.py
@@ -0,0 +1,69 @@
+from flask_restful import Resource, reqparse
+
+hoteis = [
+    {
+        "hotel_id": "alpha",
+        "nome": "Alpha Hotel",
+        "estrelas": 4.4,
+        "diaria": 320.34,
+        "cidade": "Santa Catarina"
+    },
+    {
+        "hotel_id": "bravo",
+        "nome": "Alpha Hotel",
+        "estrelas": 4.4,
+        "diaria": 320.34,
+        "cidade": "Santa Catarina"
+    }
+]
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': hoteis}
+    
+
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument("nome")
+    argumentos.add_argument("estrelas")
+    argumentos.add_argument("diaria")
+    argumentos.add_argument("cidade")
+
+
+    def find_hotel(self, hotel_id):
+        for hotel in hoteis:
+            if hotel["hotel_id"] == hotel_id:
+                return hotel
+            
+        return None
+    
+
+    def get(self, hotel_id):
+        hotel = self.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {"message": "Hotel not found."}, 404 #not found
+
+    def post(self, hotel_id):
+        dados = self.argumentos.parse_args()
+        novo_hotel = { "hotel_id": hotel_id, **dados}
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+
+    def put(self, hotel_id):
+        dados = self.argumentos.parse_args()
+        novo_hotel = { "hotel_id": hotel_id, **dados}
+
+        hotel = self.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 201
+        
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
+
+    def delete(self, hotel_id):
+        pass
+    
+    
