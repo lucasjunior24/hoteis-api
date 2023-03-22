f280a07d15a65b0f67277fe8b660a5e990b8662f
unknown <lucas11souza97@gmail.com>
2023-03-21 23:53:10 -0400

    feat: created connection with db

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
-app.config["SQL_ALCHEMY_DATABASE_URI"] = 'sqlite:///banco.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///banco.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
 
-@app.brefore_first_request
+@app.before_first_request
 def cria_banco():
     banco.create_all()
 
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,72 +1,61 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-    {
-        "hotel_id": "alpha",
-        "nome": "Alpha Hotel",
-        "estrelas": 4.4,
-        "diaria": 320.34,
-        "cidade": "Santa Catarina"
-    },
-    {
-        "hotel_id": "bravo",
-        "nome": "Alpha Hotel",
-        "estrelas": 4.4,
-        "diaria": 320.34,
-        "cidade": "Santa Catarina"
-    }
-]
-
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': hoteis}
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
     
 
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument("nome")
-    argumentos.add_argument("estrelas")
+    argumentos.add_argument("nome", type=str, required=True, help="The field 'name' cannot be left blank.")
+    argumentos.add_argument("estrelas", type=float, required=True, help="The field 'estrelas' cannot be left blank.")
     argumentos.add_argument("diaria")
     argumentos.add_argument("cidade")
 
 
-    def find_hotel(self, hotel_id):
-        for hotel in hoteis:
-            if hotel["hotel_id"] == hotel_id:
-                return hotel
-            
-        return None
-    
-
     def get(self, hotel_id):
-        hotel = self.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {"message": "Hotel not found."}, 404 #not found
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": f"Hotel id '{hotel_id}' already exists."}, 400
         dados = self.argumentos.parse_args()
-        hotel_object = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_object.json()
-        hoteis.append(novo_hotel)
-        return novo_hotel, 200
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {"message": "An internal error ocurred trying to save hotel."}, 500
+        return hotel.json(), 201
 
     def put(self, hotel_id):
         dados = self.argumentos.parse_args()
-        hotel_object = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_object.json()
-        hotel = self.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 201
+        hotel_model = HotelModel.find_hotel(hotel_id)
+        if hotel_model:
+            hotel_model.update_hotel(**dados)
+            try:
+                hotel.save_hotel()
+            except:
+                return {"message": "An internal error ocurred trying to save hotel."}, 500
+            return hotel_model.json(), 200
         
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {"message": "An internal error ocurred trying to save hotel."}, 500
+        return hotel.json(), 201
 
     def delete(self, hotel_id):
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel["hotel_id"] != hotel_id]
-        return {"message": "Hotel deleted."}
-    
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return {"message": "An internal error ocurred trying to save hotel."}, 500
+            return {"message": "Hotel deleted."}
+        return {"message": "Hotel not found."}, 404
