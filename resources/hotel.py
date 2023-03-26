633f1148c2b20fb68f2b190da9d042ac1881676d
unknown <lucas11souza97@gmail.com>
2023-03-25 20:26:19 -0400

    feat: Implemented login

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
-
+from flask_jwt_extended import jwt_required
 
 class Hoteis(Resource):
     def get(self):
@@ -21,6 +21,8 @@ class Hotel(Resource):
             return hotel.json()
         return {"message": "Hotel not found."}, 404 #not found
 
+
+    @jwt_required()
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
             return {"message": f"Hotel id '{hotel_id}' already exists."}, 400
@@ -32,6 +34,8 @@ class Hotel(Resource):
             return {"message": "An internal error ocurred trying to save hotel."}, 500
         return hotel.json(), 201
 
+
+    @jwt_required()
     def put(self, hotel_id):
         dados = self.argumentos.parse_args()
         hotel_model = HotelModel.find_hotel(hotel_id)
@@ -50,6 +54,8 @@ class Hotel(Resource):
             return {"message": "An internal error ocurred trying to save hotel."}, 500
         return hotel.json(), 201
 
+
+    @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
--- resources/user.py
+++ resources/user.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+from flask_jwt_extended import create_access_token, jwt_required
+import hmac
+
 
 class User(Resource):
     def get(self, user_id):
@@ -9,6 +12,7 @@ class User(Resource):
         return {"message": "User not found."}, 404 #not found
     
 
+    @jwt_required()
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
@@ -37,12 +41,17 @@ class UserRegister(Resource):
         return {"message": "User created successefully."}, 201
     
 
-    def delete(self, user_id):
-        user = UserModel.find_user(user_id)
-        if user:
-            try:
-                user.delete_user()
-            except:
-                return {"message": "An internal error ocurred trying to save user."}, 500
-            return {"message": "User deleted."}
-        return {"message": "User not found."}, 404
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument("email", type=str, required=True, help="The field 'e-mail' cannot be left blank.")
+        atributos.add_argument("senha", type=str, required=True, help="The field 'senha' cannot be left blank.")
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_email(dados["email"])
+        if user and hmac.compare_digest(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {"acess_toekn": token_de_acesso}, 200 #not found
+        return {"message": "The username or password is incorrect."}, 401
+    
